24638bc008598b4c0ab71271aa5f6f4cd8f4f166
haoke.wu@gmail.com <haoke.wu@gmail.com>
2021-12-27 22:38:34 +0800

    modify

--- src/main/java/com/hk/dao/OrderUserListMapper.java
+++ src/main/java/com/hk/dao/OrderUserListMapper.java
@@ -3,6 +3,10 @@ package com.hk.dao;
 import java.util.List;
 
 public interface OrderUserListMapper {
+    /**
+     * order user orderDetail   union search
+     * @return
+     */
     public List findOrderUserDetailListMapper();
     public List findUserOrderDetailItemListMapper();
     public List findOrderUserListLazyLoading();
