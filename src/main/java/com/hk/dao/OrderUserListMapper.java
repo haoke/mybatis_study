c421ec11e3326c7e159af6d2150d08af15312ea4
hulk <haoke.wu@gmail.com>
2022-01-12 01:01:59 +0800

    generic type

--- src/main/java/com/hk/dao/OrderUserListMapper.java
+++ src/main/java/com/hk/dao/OrderUserListMapper.java
@@ -1,5 +1,6 @@
 package com.hk.dao;
 
+import com.hk.pojo.Order;
 import com.hk.pojo.User;
 
 import java.util.List;
@@ -9,18 +10,18 @@ public interface OrderUserListMapper {
      *  多对一 order->user->orderDetail
      * @return
      */
-    public List findOrderUserDetailListMapper();
+    public List<Order> findOrderUserDetailListMapper();
 
     /**
      * 一对多 user->order->orderdetail->item
      */
-    public List findUserOrderDetailItemListMapper();
+    public List<User> findUserOrderDetailItemListMapper();
 
     /**
      * 多对一 分步查询 且 延迟加载
      * @return
      */
-    public List findOrderUserListLazyLoading();
+    public List<Order> findOrderUserListLazyLoading();
 
     /**
      * 一对多分步查询
